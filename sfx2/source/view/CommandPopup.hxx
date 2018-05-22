9bd5a1d5d797db719f405ebf6a6011185b90c35b
Tomaž Vajngerl <tomaz.vajngerl@collabora.co.uk>
2018-05-22 14:44:39 +0900

    Command Popup
    Change-Id: I92cdd3130b8de42ee0863c9e7154e7c7246d9377

---
+++ sfx2/source/view/CommandPopup.hxx
@@ -0,0 +1,65 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#pragma once
+
+#include <vcl/layout.hxx>
+
+#include <sfx2/dllapi.h>
+#include <sfx2/viewfrm.hxx>
+#include <vcl/floatwin.hxx>
+
+#include <com/sun/star/container/XNameAccess.hpp>
+
+#include <vcl/InterimItemWindow.hxx>
+#include <vcl/weld.hxx>
+#include <vcl/window.hxx>
+
+struct CurrentEntry;
+class MenuContentHandler;
+
+class SFX2_DLLPUBLIC CommandPopup : public FloatingWindow
+{
+public:
+    explicit CommandPopup(vcl::Window* pWorkWindow);
+
+    ~CommandPopup() override;
+
+    void dispose() override;
+};
+
+class SFX2_DLLPUBLIC CommandListBox final : public InterimItemWindow
+{
+private:
+    CommandPopup& m_rPopUp;
+
+    std::unique_ptr<weld::Entry> m_pEntry;
+    std::unique_ptr<weld::TreeView> m_pCommandTreeView;
+    std::unique_ptr<weld::Label> m_pLabel;
+
+    std::vector<CurrentEntry> m_aCommandList;
+    OUString m_PreviousText;
+    std::unique_ptr<MenuContentHandler> m_pMenuContentHandler;
+
+    DECL_LINK(ModifyHdl, weld::Entry&, void);
+    DECL_LINK(SelectionChanged, weld::TreeView&, void);
+
+    void dispatchCommandAndClose(OUString const& rCommand);
+    bool EventNotify(NotifyEvent& rNotifyEvent) override;
+
+public:
+    CommandListBox(vcl::Window* pParent, CommandPopup& rPopUp,
+                   css::uno::Reference<css::frame::XFrame> const& xFrame);
+
+    void initialize();
+
+    void dispose() override;
+};
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
